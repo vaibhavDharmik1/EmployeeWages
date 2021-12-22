9987f61868d1b85ce122a6352d7cbac7244034d7
vaibhavDharmik1 <vaibhavdharmik55.com>
2021-12-21 15:26:38 -1100

    Added code to calculate wage for 100 working hours

---
+++ EmpwagesformonthUC6.java
@@ -0,0 +1,39 @@
+public class EmpwagesformonthUC6 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
+	public static void main(String[] args) {
+		System.out.println("Employee Wage Compuatation problem");
+		
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		int totalEmpWage = 0;
+		
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+
+			}
+
+			System.out.println("Day#:" + totalWorkingDays + "emp Hr " + empHrs);
+			totalEmpHrs = totalEmpHrs + empHrs;
+		}
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp HR: " + totalEmpHrs);
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+	}
+}
